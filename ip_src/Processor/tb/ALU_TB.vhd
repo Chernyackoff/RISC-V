bb87b6ed030c25f39d76d31861bec0b922168d65
Daniel <j.daniil@bk.ru>
2025-03-01 14:02:06 +0300

    Corrected comments in ALU_TB.vhd

--- ip_src/Processor/tb/ALU_TB.vhd
+++ ip_src/Processor/tb/ALU_TB.vhd
@@ -80,7 +80,7 @@ PORT MAP (
             end loop;
 
         end loop;
-        opcode <= "010"; -- Set 1 if b (immediate) > a. All parameter are unsigned
+        opcode <= "010"; -- Set 1 if b (immediate) > a. All parameter are signed
         wait for 15 us;
         for i in 0 to 5 loop
             a <= to_unsigned(i, a'length); -- all values of the first operator
