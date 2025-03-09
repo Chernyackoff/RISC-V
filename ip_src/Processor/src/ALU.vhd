6a98e878b73445d4047b7666ee20b4457e7cd013
Daniel <j.daniil@bk.ru>
2025-03-09 15:30:41 +0300

    Corrected ALU and added multiplexer

--- ip_src/Processor/src/ALU.vhd
+++ ip_src/Processor/src/ALU.vhd
@@ -4,15 +4,18 @@ USE IEEE.numeric_std.ALL;--! for the signed, unsigned types and arithmetic ops
 
 entity ALU is
 GENERIC (
-    ALU_DATA_WIDTH: INTEGER := 32;
-    ALU_OPCODE_WIDTH: INTEGER := 3
+    ALU_DATA_WIDTH: INTEGER := 32;-- Data lines amount
+    ALU_FUNCT3_WIDTH: INTEGER := 3;-- funct3 signal lines amount
+    ALU_FUNCT7_WIDTH: INTEGER := 7-- funct7 signal lines amount
 );
 PORT (
     rst    : IN STD_LOGIC;--! sync active high reset. sync -> refclk
-    alu_input_a : IN unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
-    alu_input_b : IN unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
-    alu_output : OUT unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
-    alu_opcode : IN unsigned(ALU_OPCODE_WIDTH - 1 DOWNTO 0) := (OTHERS => '0')
+    clk    : IN STD_LOGIC;--! input clock signal
+    alu_input_a : IN unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');--! input from regfile
+    alu_input_b : IN unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');--! input from multiplexer
+    alu_output : OUT unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');--! output
+    alu_funct3 : IN unsigned(ALU_FUNCT3_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');--! input funct3 field from command
+    alu_funct7 : IN unsigned(ALU_FUNCT7_WIDTH - 1 DOWNTO 0) := (OTHERS => '0')--! input funct7 field from command
 );
 END entity ALU;
 
@@ -20,33 +23,47 @@ END entity ALU;
 architecture rtl of ALU is
     SIGNAL out_val : unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
 BEGIN
-main_proc : PROCESS(alu_opcode, alu_input_a, alu_input_b) is
-BEGIN
-    if(rst = '0') then
-        case (alu_opcode) is
-            when "000" => out_val <= alu_input_a + alu_input_b; -- a + b
-            when "001" => out_val <= alu_input_a sll to_integer(alu_input_b); -- a << b
-            when "100" => out_val <= alu_input_a xor alu_input_b; -- a xor b
-            when "101" => out_val <= alu_input_a srl to_integer(alu_input_b); -- a >> b
-            when "110" => out_val <= alu_input_a or alu_input_b; -- a or b
-            when "111" => out_val <= alu_input_a and alu_input_b; -- a and b
-            when others => out_val <= (OTHERS => '0');
-        end case;
-        if(alu_opcode = "010") then -- Set 1 if b (immediate) > a. All parameter are signed
-            if(signed(alu_input_b) > signed(alu_input_a)) then
-                out_val <= to_unsigned(1, out_val'length);
-            else 
-                out_val <= to_unsigned(0, out_val'length);
+    main_proc : PROCESS(clk) is
+    BEGIN
+        if(rst = '0') then
+            if(rising_edge(clk)) then
+                case (alu_funct3) is
+                    when "001" => out_val <= alu_input_a sll to_integer(alu_input_b); -- a << b
+                    when "100" => out_val <= alu_input_a xor alu_input_b; -- a xor b
+                    when "110" => out_val <= alu_input_a or alu_input_b; -- a or b
+                    when "111" => out_val <= alu_input_a and alu_input_b; -- a and b
+                    when others => out_val <= (OTHERS => '0');
+                end case;
+                if(alu_funct3 = "000") then
+                    if(alu_funct7(ALU_FUNCT7_WIDTH - 2) = '0') then
+                        out_val <= alu_input_a + alu_input_b; -- a + b
+                    else
+                        out_val <= alu_input_a - alu_input_b; -- a - b
+                    end if;
+                end if;
+                if(alu_funct3 = "101") then
+                    if(alu_funct7(ALU_FUNCT7_WIDTH - 2) = '0') then
+                        out_val <= alu_input_a srl to_integer(alu_input_b); -- a >> b (logic)
+                    else
+                        out_val <= unsigned (to_stdlogicvector(to_bitvector(std_logic_vector(alu_input_a)) sra to_integer(alu_input_b))); -- a >> b (aritmetical)
+                    end if;
+                end if;
+                if(alu_funct3 = "010") then -- Set 1 if b (immediate) > a. All parameter are signed
+                    if(signed(alu_input_b) > signed(alu_input_a)) then
+                        out_val <= to_unsigned(1, out_val'length);
+                    else 
+                        out_val <= to_unsigned(0, out_val'length);
+                    end if;
+                end if;
+                if(alu_funct3 = "011") then -- Set 1 if b (immediate) > a. All parameter are unsigned
+                    if(alu_input_b > alu_input_a) then
+                        out_val <= to_unsigned(1, out_val'length);
+                    else 
+                        out_val <= to_unsigned(0, out_val'length);
+                    end if;
+                end if;
+                alu_output <= out_val;
             end if;
         end if;
-        if(alu_opcode = "011") then -- Set 1 if b (immediate) > a. All parameter are unsigned
-            if(alu_input_b > alu_input_a) then
-                out_val <= to_unsigned(1, out_val'length);
-            else 
-                out_val <= to_unsigned(0, out_val'length);
-            end if;
-        end if;
-        alu_output <= out_val;
-    end if;
-end process main_proc;
+    end process main_proc;
 END architecture rtl;
--- ip_src/Processor/tb/ALU_TB.vhd
+++ ip_src/Processor/tb/ALU_TB.vhd
@@ -5,23 +5,27 @@ USE IEEE.numeric_std.ALL;--! for the signed, unsigned types and arithmetic ops
 
 entity ALU_TB is
 GENERIC (
-  DATA_WIDTH : INTEGER := 32;
-  OPCODE_WIDTH :INTEGER := 3
+    DATA_WIDTH : INTEGER := 32; --! Data lines amount
+    FUNCT3_WIDTH: INTEGER := 3; --! funct3 signal lines amount
+    FUNCT7_WIDTH: INTEGER := 7 --! funct7 signal lines amount
 );
 end entity ALU_TB;
 
 architecture rtl of ALU_TB is
     component ALU is
     GENERIC (
-        ALU_DATA_WIDTH: INTEGER := 32;
-        ALU_OPCODE_WIDTH: INTEGER := 3
+        ALU_DATA_WIDTH: INTEGER := 32;--! Data lines amount
+        ALU_FUNCT3_WIDTH: INTEGER := 3;--! funct3 signal lines amount
+        ALU_FUNCT7_WIDTH: INTEGER := 7--! funct7 signal lines amount
     );
     PORT (
         rst    : IN STD_LOGIC;--! sync active high reset. sync -> refclk
-        alu_input_a : IN unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0);
-        alu_input_b : IN unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0);
-        alu_output : OUT unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0);
-        alu_opcode : IN unsigned(ALU_OPCODE_WIDTH - 1 DOWNTO 0)
+        clk    : IN STD_LOGIC;--! input clock signal
+        alu_input_a : IN unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0);--! input from regfile
+        alu_input_b : IN unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0);--! input from multiplexer
+        alu_output : OUT unsigned(ALU_DATA_WIDTH - 1 DOWNTO 0);--! output
+        alu_funct3 : IN unsigned(ALU_FUNCT3_WIDTH - 1 DOWNTO 0);--! input funct3 field from command
+        alu_funct7 : IN unsigned(ALU_FUNCT7_WIDTH - 1 DOWNTO 0)--! input funct7 field from command
     );
     END component;
     SIGNAL clk : STD_LOGIC := '0';
@@ -29,21 +33,25 @@ architecture rtl of ALU_TB is
     SIGNAL a : unsigned(DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
     SIGNAL b : unsigned(DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
     SIGNAL c : unsigned(DATA_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL opcode : unsigned(OPCODE_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL funct3 : unsigned(FUNCT3_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL funct7 : unsigned(FUNCT7_WIDTH - 1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL a1 : signed (DATA_WIDTH - 1 DOWNTO 0) := x"FFFFFFF9";
     signal test_completed: boolean := false;
 begin
 
 alu_inst: ALU 
 GENERIC MAP (
     ALU_DATA_WIDTH => DATA_WIDTH,
-    ALU_OPCODE_WIDTH => OPCODE_WIDTH
+    ALU_FUNCT3_WIDTH => FUNCT3_WIDTH
 )
 PORT MAP (
     rst => rst,
+    clk => clk,
     alu_input_a => a,
     alu_input_b => b,
     alu_output => c,
-    alu_opcode => opcode
+    alu_funct3 => funct3,
+    alu_funct7 => funct7
 );
 
     clk_gen: process is
@@ -58,7 +66,8 @@ PORT MAP (
     begin
         rst <= '0';
         wait for 15 us;
-        opcode <= "000"; -- add operation
+        funct7 <= (OTHERS => '0');
+        funct3 <= "000"; -- add operation
         wait for 15 us;
         for i in 0 to 5 loop
             a <= to_unsigned(i, a'length); -- all values of the first operator
@@ -69,7 +78,9 @@ PORT MAP (
             end loop;
 
         end loop;
-        opcode <= "001"; -- logical left shift 
+        wait for 15 us;
+        funct7(funct7'length - 2) <= '1';
+        funct3 <= "000"; -- sub operation
         wait for 15 us;
         for i in 0 to 5 loop
             a <= to_unsigned(i, a'length); -- all values of the first operator
@@ -80,8 +91,9 @@ PORT MAP (
             end loop;
 
         end loop;
-        opcode <= "010"; -- Set 1 if b (immediate) > a. All parameter are signed
         wait for 15 us;
+        funct7 <= (OTHERS => '0');
+        funct3 <= "001"; -- logical left shift 
         for i in 0 to 5 loop
             a <= to_unsigned(i, a'length); -- all values of the first operator
             wait for 15 us;
@@ -91,8 +103,8 @@ PORT MAP (
             end loop;
 
         end loop;
-        opcode <= "011"; -- Set 1 if b (immediate) > a. All parameter are unsigned
         wait for 15 us;
+        funct3 <= "010"; -- Set 1 if b (immediate) > a. All parameter are signed
         for i in 0 to 5 loop
             a <= to_unsigned(i, a'length); -- all values of the first operator
             wait for 15 us;
@@ -102,8 +114,8 @@ PORT MAP (
             end loop;
 
         end loop;
-        opcode <= "100"; -- xor
         wait for 15 us;
+        funct3 <= "011"; -- Set 1 if b (immediate) > a. All parameter are unsigned
         for i in 0 to 5 loop
             a <= to_unsigned(i, a'length); -- all values of the first operator
             wait for 15 us;
@@ -113,8 +125,8 @@ PORT MAP (
             end loop;
 
         end loop;
-        opcode <= "101"; -- logical shift right
         wait for 15 us;
+        funct3 <= "100"; -- xor
         for i in 0 to 5 loop
             a <= to_unsigned(i, a'length); -- all values of the first operator
             wait for 15 us;
@@ -124,8 +136,8 @@ PORT MAP (
             end loop;
 
         end loop;
-        opcode <= "110"; -- or
         wait for 15 us;
+        funct3 <= "101"; -- logical shift right
         for i in 0 to 5 loop
             a <= to_unsigned(i, a'length); -- all values of the first operator
             wait for 15 us;
@@ -135,8 +147,18 @@ PORT MAP (
             end loop;
 
         end loop;
-        opcode <= "111"; -- and
         wait for 15 us;
+        funct7(funct7'length - 2) <= '1';
+        funct3 <= "101"; -- arithmetic shift right
+        a <= unsigned(a1); -- all values of the first operator
+        wait for 15 us;
+        for j in 0 to 5 loop
+            b <= to_unsigned(j, b'length); -- all values of the second operator
+            wait for 15 us;
+        end loop;
+        wait for 15 us;
+        funct7 <= (OTHERS => '0');
+        funct3 <= "110"; -- or
         for i in 0 to 5 loop
             a <= to_unsigned(i, a'length); -- all values of the first operator
             wait for 15 us;
@@ -145,6 +167,16 @@ PORT MAP (
                 wait for 15 us;
             end loop;
 
+        end loop;
+        wait for 15 us;
+        funct3 <= "111"; -- and
+        for i in 0 to 5 loop
+            a <= to_unsigned(i, a'length); -- all values of the first operator
+            wait for 15 us;
+            for j in 0 to 5 loop
+                b <= to_unsigned(j, b'length); -- all values of the second operator
+                wait for 15 us;
+            end loop;
         end loop;
         test_completed <= true after 15 us;
         wait;
