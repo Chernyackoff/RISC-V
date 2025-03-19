49b4b14fc57ac4c5b71df94fb206fac1478d7e97
Vladislav <chernyakov_v@vk.com>
2025-03-19 23:24:14 +0300

    AXI Master reader tested in block design and should work

--- ip_src/AXI4_Master/src/AXI4_Master_TOP.vhd
+++ ip_src/AXI4_Master/src/AXI4_Master_TOP.vhd
@@ -5,15 +5,15 @@ USE IEEE.numeric_std.ALL;--! for the signed, unsigned types and arithmetic ops
 
 ENTITY AXI4_Master_TOP IS
   GENERIC (
-    ADDR_WIDTH : INTEGER = 32;
-    DATA_WIDTH : INTEGER = 32;
+    ADDR_WIDTH : INTEGER := 32;
+    DATA_WIDTH : INTEGER := 32
   );
   PORT (
     refclk : IN STD_LOGIC;--! reference clock expect 250Mhz
     arst_n : IN STD_LOGIC;--! sync active low reset. sync -> refclk
 
     --! Controll ports 
-    i_addr     : IN STD_LOGIC_VECTOR(ADDR_WIDTH - 1 DOWNTO 0)
+    i_addr     : IN STD_LOGIC_VECTOR(ADDR_WIDTH - 1 DOWNTO 0);
     i_data     : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
     i_read_req : IN STD_LOGIC;
     i_ready    : IN STD_LOGIC;
@@ -51,7 +51,7 @@ ENTITY AXI4_Master_TOP IS
 
     -- read address signals 
     M_AXI_ARID    : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
-    M_AXI_ARADDR  : OUT STD_LOGIC_VECTOR;
+    M_AXI_ARADDR  : OUT STD_LOGIC_VECTOR(31 downto 0);
     M_AXI_ARLEN   : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
     M_AXI_ARSIZE  : OUT STD_LOGIC_VECTOR(2 DOWNTO 0);
     M_AXI_ARBURST : OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
@@ -65,7 +65,7 @@ ENTITY AXI4_Master_TOP IS
 
     -- read data signals
     M_AXI_RID   : IN STD_LOGIC_VECTOR(2 DOWNTO 0);
-    M_AXI_RDATA : IN STD_LOGIC_VECTOR(DATAWIDTH - 1 DOWNTO 0);
+    M_AXI_RDATA : IN STD_LOGIC_VECTOR(DATA_WIDTH - 1 DOWNTO 0);
     M_AXI_RRESP : IN STD_LOGIC_VECTOR(1 DOWNTO 0);
     M_AXI_RLAST : IN STD_LOGIC;
 
@@ -92,23 +92,23 @@ BEGIN
   M_AXI_ARPROT <= (OTHERS => '0'); -- priority of transaction (optional)
   M_AXI_ARID   <= (OTHERS => '0'); -- ID of transaction (not nessessary)
 
-  M_AXI_ARCACHE <= <= (OTHERS => '0'); --memory type (will use Device Non-bufferable)
+  M_AXI_ARCACHE <= (OTHERS => '0'); --memory type (will use Device Non-bufferable)
 
   -----------------------------------------------------------------------
   -- read automatons
   -----------------------------------------------------------------------
-  read_sm_handler : PROCESS (clk)
+  read_sm_handler : PROCESS (refclk)
   BEGIN
-    IF rising_edge(clk) THEN
-      IF rst = '0' THEN
-        cur_r_ctrl_state <= idle;
+    IF rising_edge(refclk) THEN
+      IF arst_n = '0' THEN
+        cur_r_ctrl_state <= st_idle;
         go_ar            <= '0';
         go_r             <= '0';
       ELSE
         CASE cur_r_ctrl_state IS
           WHEN st_address =>
             go_r <= '0';
-            IF done_ar THEN
+            IF done_ar = '1' THEN
               cur_r_ctrl_state <= st_data;
               go_ar            <= '0';
             ELSE
@@ -118,7 +118,7 @@ BEGIN
 
           WHEN st_data =>
             go_ar <= '0';
-            IF done_r THEN
+            IF done_r = '1' THEN
               cur_r_ctrl_state <= st_idle;
               go_r             <= '0';
             ELSE
@@ -129,7 +129,7 @@ BEGIN
           WHEN OTHERS =>
             go_ar <= '0';
             go_r  <= '0';
-            IF i_read_req THEN
+            IF i_read_req = '1' THEN
               cur_r_ctrl_state <= st_address;
             ELSE
               cur_r_ctrl_state <= st_idle;
@@ -139,11 +139,11 @@ BEGIN
     END IF;
   END PROCESS;
 
-  address_read_channel : PROCESS (clk)
+  address_read_channel : PROCESS (refclk)
   BEGIN
-    IF rising_edge(clk) THEN
-      IF rst = '0' THEN
-        cur_ar_state  <= IDLE;
+    IF rising_edge(refclk) THEN
+      IF arst_n = '0' THEN
+        cur_ar_state  <= st_idle;
         M_AXI_ARADDR  <= (OTHERS => '0');
         M_AXI_ARLEN   <= (OTHERS => '0');
         M_AXI_ARSIZE  <= (OTHERS => '0');
@@ -156,11 +156,11 @@ BEGIN
           WHEN st_busy =>
             done_ar       <= '0';
             M_AXI_ARADDR  <= i_addr;
-            M_AXI_ARLEN   <= 63;    -- from docs : "Burst_Length = AxLEN[7:0] + 1, to accommodate the extended burst length of the INCR burst type in AXI4"
-            M_AXI_ARSIZE  <= '101'; -- 32-bit bus
-            M_AXI_ARBURST <= '01';  -- increment address on every data burst
+            M_AXI_ARLEN   <=  std_logic_vector(to_unsigned (63, 8)); -- IN VHDL2008 : 8D"63";    -- from docs : "Burst_Length = AxLEN[7:0] + 1, to accommodate the extended burst length of the INCR burst type in AXI4"
+            M_AXI_ARSIZE  <= B"101"; -- 32-bit bus
+            M_AXI_ARBURST <= B"01";  -- increment address on every data burst
             M_AXI_ARVALID <= '1';
-            IF M_AXI_ARREADY THEN
+            IF M_AXI_ARREADY = '1' THEN
               cur_ar_state <= st_done;
             ELSE
               cur_ar_state <= st_busy;
@@ -175,13 +175,13 @@ BEGIN
             done_ar       <= '1';
             cur_ar_state  <= st_idle;
 
-          WHEN OTHERS => -- define it as idle )
+          WHEN OTHERS => -- define it as idle 
             M_AXI_ARADDR  <= (OTHERS => '0');
             M_AXI_ARLEN   <= (OTHERS => '0');
             M_AXI_ARSIZE  <= (OTHERS => '0');
             M_AXI_ARBURST <= (OTHERS => '0');
             M_AXI_ARVALID <= '0';
-            IF go_ar THEN
+            IF go_ar = '1' THEN
               cur_ar_state <= st_busy;
             ELSE
               cur_ar_state <= st_idle;
@@ -192,24 +192,24 @@ BEGIN
     END IF;
   END PROCESS;
 
-  read_data_channel : PROCESS (clk)
+  read_data_channel : PROCESS (refclk)
   BEGIN
-    IF rising_edge(clk) THEN
-      IF rst = '0' THEN
+    IF rising_edge(refclk) THEN
+      IF arst_n = '0' THEN
         cur_r_state  <= st_idle;
         M_AXI_RREADY <= '0';
         o_data       <= (OTHERS => '0');
         o_valid      <= '0';
         data_counter <= 0;
-        done_ar      <= '0';
+        done_r      <= '0';
       ELSE
-        CASE state IS
+        CASE cur_r_state IS
           WHEN st_busy =>
-            done_ar      <= '0';
+            done_r      <= '0';
             M_AXI_RREADY <= '0';
             IF data_counter < 64 THEN
-              IF M_AXI_RVALID AND M_AXI_RRESP ~ = '10' AND M_AXI_RESP ~ = '11'
-                IF i_ready
+              IF M_AXI_RVALID = '1' AND M_AXI_RRESP /= "10" AND M_AXI_RRESP /= "11" then
+                IF i_ready ='1' then
                   M_AXI_RREADY <= '1';
                   o_data       <= M_AXI_RDATA;
                   o_valid      <= '1';
@@ -219,7 +219,7 @@ BEGIN
                   o_data       <= M_AXI_RDATA;
                   o_valid      <= '1';
                 END IF;
-                IF M_AXI_RLAST
+                IF M_AXI_RLAST = '1' then
                   cur_r_state <= st_done;
                 ELSE
                   cur_r_state <= st_busy;
@@ -241,7 +241,7 @@ BEGIN
             o_data       <= (OTHERS => '0');
             o_valid      <= '0';
             data_counter <= 0;
-            done_ar      <= '1';
+            done_r      <= '1';
             cur_r_state  <= st_idle;
 
           WHEN OTHERS =>
@@ -249,8 +249,8 @@ BEGIN
             o_data       <= (OTHERS => '0');
             o_valid      <= '0';
             data_counter <= 0;
-            done_ar      <= '0';
-            IF go_r THEN
+            done_r      <= '0';
+            IF go_r = '1' THEN
               cur_r_state <= st_busy;
             ELSE
               cur_r_state <= st_idle;
