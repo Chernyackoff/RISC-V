91ba17196c98165e1fa83385e81b783eb69367b8
Chernyackoff <chernyackoff.vlad@yandex.ru>
2025-03-20 14:26:58 +0300

    TCL scripts for cache and AXI IP packaging

--- ip_src/Cache/tcl/package_project.tcl
+++ ip_src/Cache/tcl/package_project.tcl
@@ -1,15 +1,15 @@
 update_compile_order -fileset sources_1
-ipx::package_project -root_dir $IP_PATH/AXI_Master -vendor SUAI -library user -taxonomy /UserIP -import_files -set_current false
+ipx::package_project -root_dir $IP_PATH/Cache -vendor SUAI -library user -taxonomy /UserIP -import_files -set_current false
 
-ipx::unload_core $IP_PATH/AXI_Master/component.xml
-ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory $IP_PATH/AXI_Master $IP_PATH/AXI_Master/component.xml
+ipx::unload_core $IP_PATH/Cache/component.xml
+ipx::edit_ip_in_project -upgrade true -name tmp_edit_project -directory $IP_PATH/Cache $IP_PATH/Cache/component.xml
 
 update_compile_order -fileset sources_1
-set_property display_name AXI4_Master [ipx::current_core]
-set_property description {AXI4_Master is master for AXI MM} [ipx::current_core]
+set_property display_name Cache [ipx::current_core]
+set_property description {Cache for our SoC RISC-V processor} [ipx::current_core]
 set_property vendor_display_name SUAI [ipx::current_core]
 set_property vendor SUAI [ipx::current_core]
-set_property name AXI4_Master [ipx::current_core]
+set_property name Cache [ipx::current_core]
 set_property version 0.2 [ipx::current_core]
 set_property supported_families {qvirtex7 Production versal Production kintex7 Production kintex7l Production qkintex7 Production qkintex7l Production akintex7 Production artix7 Production artix7l Production aartix7 Production qartix7 Production zynq Production qzynq Production azynq Production spartan7 Production aspartan7 Production virtexu Production zynquplus Production virtexuplus Production virtexuplusHBM Production virtexuplus58g Production kintexuplus Production artixuplus Production kintexu Production} [ipx::current_core]
 set_property supported_families {qvirtex7 Production versal Production artix7 Production artix7l Production aartix7 Production qartix7 Production zynq Production qzynq Production azynq Production spartan7 Production aspartan7 Production virtexu Production zynquplus Production virtexuplus Production virtexuplusHBM Production virtexuplus58g Production kintexuplus Production artixuplus Production kintexu Production} [ipx::current_core]
@@ -17,7 +17,7 @@ set_property supported_families {qvirtex7 Production versal Production virtexu P
 set_property supported_families {qvirtex7 Production versal Production virtexu Production zynquplus Production kintexuplus Production artixuplus Production kintexu Production} [ipx::current_core]
 set_property supported_families {qvirtex7 Production virtexu Production zynquplus Production kintexuplus Production artixuplus Production kintexu Production} [ipx::current_core]
 set_property supported_families {qvirtex7 Production virtexu Production zynquplus Production kintexuplus Production artixuplus Production kintexu Production kintex7 Beta kintex7l Beta kintexuplus Beta kintexu Beta} [ipx::current_core]
-set_property previous_version_for_upgrade xilinx.com:user:AXI4_Master_TOP:1.0 [ipx::current_core]
+
 set_property core_revision 1 [ipx::current_core]
 ipx::create_xgui_files [ipx::current_core]
 ipx::update_checksums [ipx::current_core]
